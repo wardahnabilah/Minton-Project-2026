4a2c248ea7ecfc8932adf50091e0e702c3ef8e0e
wardahnabilah <119636138+wardahnabilah@users.noreply.github.com>
2026-06-08 23:47:07 +0700

    chore: change component name

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -4,7 +4,7 @@ export function AdminPage() {
     return (
         <section className="p-28 mx-auto">
             <h1 className="text-3xl text-center text-white font-bold uppercase tracking-wider">Settings</h1>
-            <ButtonMenu pathName="/admin/courts" title="Courts" />
+            <MenuButton pathName="/admin/courts" title="Courts" />
         </section>
     );
 }
