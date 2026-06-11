4d6c641048742d76eb3ff6ae60d736f927bfc94f
wardahnabilah <119636138+wardahnabilah@users.noreply.github.com>
2026-06-11 20:54:59 +0700

    chore: delete unnecessary code

--- src/components/modals/courts/CreateEditModal.jsx
+++ src/components/modals/courts/CreateEditModal.jsx
@@ -3,12 +3,7 @@ import { ButtonFilled } from "../../elements/Buttons";
 import { AuthContext } from "../../../context/AuthContext";
 import { CourtForm } from "../../forms/CourtForm";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 export function CreateEditModal({ courtId, courtName = null, isOpen, closeModal, getCourts, children }) {
-    const {loggedInUser} = useContext(AuthContext);
-    const [court, setCourt] = useState();
-
     return (
         <div className={`fixed inset-0 flex justify-center items-center transition-colors bg-black/60`}>
             <div className="w-90 h-50 bg-white text-primary-dark rounded-2xl shadow px-6 py-4 transition-all">
